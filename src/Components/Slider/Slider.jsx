c2f104d0824e3c3a8cd6c0cb2f2d181c70897c17
jorge <monterrosaspix@gmail.com>
2022-07-07 01:55:07 -0500

    Fixing render error on Slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -4,37 +4,43 @@ import SliderButton from './SliderButton/SliderButton';
 import SliderContainer from './SliderStyled';
 
 const Slider = () => {
-    const {data, isLoading} = useFeaturedBanners();
-    const [currentBannerIndex, setCurrentBanner] = useState(0);
+    const [currentBannerIndex, setCurrentBannerIndex] = useState();
     const [currentImageUrl, setCurrrentImageUrl] = useState('');
     const [currentImageAlt, setCurrentImageAlt] = useState('An image');   
+    const {data, isLoading} = useFeaturedBanners();
+
 
     useEffect(
         ()=>{
             if(!isLoading){
                 const bannersArray = [...data.results];
+                //No se mueve porque debe triggereas a banneridx para que los demas se actualicen
+                //Refactor para que pueda detectar un cambio
+                // setCurrentBannerIndex();
                 setCurrrentImageUrl(bannersArray[currentBannerIndex].data.main_image.url);
                 setCurrentImageAlt(bannersArray[currentBannerIndex].data.main_image.alt);
-                console.log(data, isLoading);
+            }
+            else{
+                console.log('aun no carga');
             }
         }
-        ,[currentImageUrl, currentBannerIndex]);
+        ,[currentImageUrl, currentImageAlt, currentBannerIndex]);
 
     const nextBanner = () => {
         if(currentBannerIndex < data.results.length - 1){
-            setCurrentBanner(currentBannerIndex + 1);
+            setCurrentBannerIndex(currentBannerIndex + 1);
         }
         else{
-            setCurrentBanner(0);
+            setCurrentBannerIndex(0);
         }
     }
 
     const prevBanner = () => {
         if(currentBannerIndex > 0){
-            setCurrentBanner(currentBannerIndex - 1);
+            setCurrentBannerIndex(currentBannerIndex - 1);
         }
         else{
-            setCurrentBanner(data.results.length - 1);
+            setCurrentBannerIndex(data.results.length - 1);
         }
     }
 
